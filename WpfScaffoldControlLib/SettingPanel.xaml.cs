591b3fd6f79b107f4e936b311f683f731a7dbafc
LocalPeople <695152458@qq.com>
2017-03-24 18:01:52 +0800

    master

--- WpfScaffoldControlLib/SettingPanel.xaml.cs
+++ WpfScaffoldControlLib/SettingPanel.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Xml.Linq;
 
@@ -16,22 +17,27 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
     public partial class SettingPanel : UserControl
     {
         private SettingViewModel[] _viewModel;
-        private List<SettingViewModel> _visibleViewModel;
+        //private List<SettingViewModel> _visibleViewModel;
         internal const string INT_ERROR_TIP = "请输入整数！";
         internal const string DOUBLE_ERROR_TIP = "请输入数值！";
         internal const string STRING_ERROR_TIP = "请输入文字！";
         private bool _isSettingSaved;
+        private ListCollectionView _view;
 
         public SettingPanel()
         {
-            InitializeComponent();
-
             // 设置参数列表数据源板初始化
             _viewModel = SettingViewModel.GetAllSettingProperties(OnViewModelChanged);
-            _visibleViewModel = new List<SettingViewModel>();
-            for (int i = 0; i < SettingViewModel.OnGroundPropertiesCount; i++)
-                _visibleViewModel.Add(_viewModel[i]);
-            DataContext = _visibleViewModel;
+            //_visibleViewModel = new List<SettingViewModel>();
+            //for (int i = 0; i < SettingViewModel.OnGroundPropertiesCount; i++)
+            //    _visibleViewModel.Add(_viewModel[i]);
+            //DataContext = _visibleViewModel;
+            DataContext = _viewModel;
+
+            _view = (ListCollectionView)CollectionViewSource.GetDefaultView(DataContext);
+            _view.GroupDescriptions.Add(new PropertyGroupDescription("GroupName"));
+
+            InitializeComponent();
 
             // 项目地区下拉框数据源初始化
             comboBox1.ItemsSource = _comboBoxItems;
@@ -106,7 +112,6 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
             XElement appSettings = xDoc.Element("configuration").Element("appSettings");
             foreach (var item in _viewModel)
             {
-                if (item.IsSeparator) continue;
                 appSettings.Add(new XElement("add", new XAttribute("key", item.LongName.TrimEnd('：')), new XAttribute("value", item.Value)));
             }
             xDoc.Save(path);
@@ -152,7 +157,6 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
                     System.Configuration.ConfigurationManager.OpenMappedExeConfiguration(configFile, System.Configuration.ConfigurationUserLevel.None);
                 for (int i = 0; i < _viewModel.Length; i++)
                 {
-                    if (_viewModel[i].IsSeparator) continue;
                     _viewModel[i].Value = cfa.AppSettings.Settings[_viewModel[i].LongName.TrimEnd('：')].Value;
                 }
                 itemsControl1.Items.Refresh();
@@ -163,29 +167,37 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
         #region 更改脚手架设计相关行为函数
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            for (int i = SettingViewModel.OnGroundPropertiesCount; i < _viewModel.Length; i++)
-                _visibleViewModel.Add(_viewModel[i]);
-            itemsControl1.Items.Refresh();
+            //for (int i = SettingViewModel.OnGroundPropertiesCount; i < _viewModel.Length; i++)
+            //    _visibleViewModel.Add(_viewModel[i]);
+            //itemsControl1.Items.Refresh();
+            _view.Filter = null;
             _isSettingSaved = false;
             _isBuildOnGrond = false;
         }
 
         private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
         {
-            if (_rbInitial)
-            {
-                _rbInitial = false;
-                return;
-            }
-            _visibleViewModel.RemoveRange(SettingViewModel.OnGroundPropertiesCount, _viewModel.Length - SettingViewModel.OnGroundPropertiesCount);
-            itemsControl1.Items.Refresh();
+            //if (_rbInitial)
+            //{
+            //    _rbInitial = false;
+            //    return;
+            //}
+            //_visibleViewModel.RemoveRange(SettingViewModel.OnGroundPropertiesCount, _viewModel.Length - SettingViewModel.OnGroundPropertiesCount);
+            //itemsControl1.Items.Refresh();
+            _view.Filter = new Predicate<object>(FilterViewModel);
             _isSettingSaved = false;
             _isBuildOnGrond = true;
         }
 
+        private bool FilterViewModel(object item)
+        {
+            SettingViewModel model = item as SettingViewModel;
+            return model.GroupName != "悬挑参数";
+        }
+
         private bool _isBuildOnGrond = true;
         internal bool IsBuildOnGround { get { return _isBuildOnGrond; } }
-        private bool _rbInitial = true;
+        //private bool _rbInitial = true;
         #endregion
 
         #region 界面交互函数
@@ -195,9 +207,8 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
             values = new List<string>();
             if (_isSettingSaved)
             {
-                foreach (var item in _visibleViewModel)
+                foreach (var item in _viewModel)
                 {
-                    if (item.IsSeparator) continue;
                     keys.Add(item.ShortName);
                     values.Add(item.Value);
                 }
@@ -210,32 +221,32 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
         public Dictionary<string, string> GetStructuralProperties()
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
+            result.Add(_viewModel[0].ShortName, _viewModel[0].Value);
             result.Add(_viewModel[1].ShortName, _viewModel[1].Value);
             result.Add(_viewModel[2].ShortName, _viewModel[2].Value);
-            result.Add(_viewModel[3].ShortName, _viewModel[3].Value);
+            result.Add(_viewModel[4].ShortName, _viewModel[4].Value);
             result.Add(_viewModel[5].ShortName, _viewModel[5].Value);
-            result.Add(_viewModel[6].ShortName, _viewModel[6].Value);
-            result.Add(_viewModel[24].ShortName, _viewModel[24].Value);
+            result.Add(_viewModel[22].ShortName, _viewModel[22].Value);
             return result;
         }
         #endregion
     }
 
     #region 数据模板选择器
-    public class SettingItemTemplateSelector : DataTemplateSelector
-    {
-        public DataTemplate DefaultDataTemplate { get; set; }
-        public DataTemplate SeparatorDataTemplate { get; set; }
+    //public class SettingItemTemplateSelector : DataTemplateSelector
+    //{
+    //    public DataTemplate DefaultDataTemplate { get; set; }
+    //    public DataTemplate SeparatorDataTemplate { get; set; }
 
-        public override DataTemplate SelectTemplate(object item, DependencyObject container)
-        {
-            SettingViewModel vm = item as SettingViewModel;
-            if (!vm.IsSeparator)
-                return DefaultDataTemplate;
-            else
-                return SeparatorDataTemplate;
-        }
-    }
+    //    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    //    {
+    //        SettingViewModel vm = item as SettingViewModel;
+    //        if (!vm.IsSeparator)
+    //            return DefaultDataTemplate;
+    //        else
+    //            return SeparatorDataTemplate;
+    //    }
+    //}
     #endregion
 
     /// <summary>
@@ -243,17 +254,17 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
     /// </summary>
     public class SettingViewModel : INotifyPropertyChanged
     {
+        private string _groupName;
         private string _shortName;
         private string _longName;
         private string _value;
         private Visibility _error;
         private string _errorDesription;
-        internal static readonly int OnGroundPropertiesCount = 28;
+        internal static readonly int OnGroundPropertiesCount = 26;
 
         #region 构造函数
         internal SettingViewModel(string shortName, string longName, object value)
         {
-            IsSeparator = false;
             _shortName = shortName;
             _longName = longName;
             _value = value.ToString();
@@ -266,16 +277,14 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
             else if (valueType == typeof(string))
                 _errorDesription = SettingPanel.STRING_ERROR_TIP;
         }
-
-        internal SettingViewModel(string separator)
-        {
-            IsSeparator = true;
-            _longName = separator;
-        }
         #endregion
 
         #region 属性成员
-        public bool IsSeparator { get; private set; }
+        public string GroupName
+        {
+            get { return _groupName; }
+            set { _groupName = value; }
+        }
 
         public string ShortName
         {
@@ -340,47 +349,44 @@ namespace XcWpfControlLib.WpfScaffoldControlLib
         {
             return new SettingViewModel[]
             {
-                new SettingViewModel("布置参数"),
-                new SettingViewModel("LGZJ","立杆纵距(m)",1.5){ PropertyChanged=handler},
-                new SettingViewModel("LGHJ","立杆横距(m)",0.8){ PropertyChanged=handler},
-                new SettingViewModel("BJ","步距(m)",1.8){ PropertyChanged=handler},
-                new SettingViewModel("LQJBS","连墙件步数",2){ PropertyChanged=handler},
-                new SettingViewModel("LQJKS","连墙件跨数",2){ PropertyChanged=handler},
-                new SettingViewModel("NJJQ","内排架距墙长度(mm)",300){ PropertyChanged=handler},
-                new SettingViewModel("荷载参数"),
-                new SettingViewModel("ZJXS","扣件抗滑承载力系数",0.8){ PropertyChanged=handler},
-                new SettingViewModel("JSJYT","脚手架用途","结构脚手架"){ PropertyChanged=handler},
-                new SettingViewModel("SGHZ","施工荷载均布参数(kN/m2)",3){ PropertyChanged=handler},
-                new SettingViewModel("SGCS","同时施工层数",2){ PropertyChanged=handler},
-                new SettingViewModel("JBFY","基本风压(kN/m2)",0.3){ PropertyChanged=handler},
-                new SettingViewModel("LGFY","立杆稳定性时风压高度变化系数",0.65){ PropertyChanged=handler},
-                new SettingViewModel("LQJFY","连墙件强度时风压高度变化系数",0.65){ PropertyChanged=handler},
-                new SettingViewModel("TXXS","风荷载体型系数",1.3){ PropertyChanged=handler},
-                new SettingViewModel("LGZZ","每米立杆数承受的结构自重(kN)",0.1295){ PropertyChanged=handler},
-                new SettingViewModel("JSBLB","脚手板类别","冲压钢脚手架"){ PropertyChanged=handler},
-                new SettingViewModel("JSBZZ","脚手板自重标准值(kN/m2)",0.3){ PropertyChanged=handler},
-                new SettingViewModel("DJBLB","栏杆、挡板类别","栏杆、冲压钢挡脚板"){ PropertyChanged=handler},
-                new SettingViewModel("DJBZZ","栏杆、挡脚板自重标准值(kN/m2)",0.16){ PropertyChanged=handler},
-                new SettingViewModel("AQWZZ","安全设施与安全网自重标准值(kN/m2)",0.005){ PropertyChanged=handler},
-                new SettingViewModel("DJTLX","地基土类型","砂土"){ PropertyChanged=handler},
-                new SettingViewModel("DJCZL","地基承载力标准值(kPa)",140){ PropertyChanged=handler},
-                new SettingViewModel("JCMJ","基础底面扩展面积(m2)",0.09){ PropertyChanged=handler},
-                new SettingViewModel("JCXS","基础降低系数",1){ PropertyChanged=handler},
-                new SettingViewModel("JSCDXS","计算长度系数",1.5){ PropertyChanged=handler},
-                new SettingViewModel("LGWDXS","轴心受压立杆的稳定系数",0.188){ PropertyChanged=handler},
-                new SettingViewModel("悬挑参数"),
-                new SettingViewModel("1DSGD","该分段顶部高度(m)",14.4){ PropertyChanged=handler},
-                new SettingViewModel("XGXH","型钢型号",14){ PropertyChanged=handler},
-                new SettingViewModel("HNTBH","楼板混凝土标号","C40"){ PropertyChanged=handler},
-                new SettingViewModel("JMMJ","型钢截面面积(cm2)",21.5){ PropertyChanged=handler},
-                new SettingViewModel("LLZL","型钢理论质量(kg/m)",16.9){ PropertyChanged=handler},
-                new SettingViewModel("ZDBJ","转动半径ix(cm)",5.75){ PropertyChanged=handler},
-                new SettingViewModel("JMML","截面模量Wx(cm3)",102){ PropertyChanged=handler},
-                new SettingViewModel("ZDGL","转动惯量Ix(cm4)",712){ PropertyChanged=handler},
-                new SettingViewModel("1LQJFY","本段顶部风压高度变化系数",0.65){ PropertyChanged=handler},
-                new SettingViewModel("MGXTBZ","锚固段长度与悬挑段长度比值",1.3){ PropertyChanged=handler},
-                new SettingViewModel("HNTKLQD","混凝土抗拉强度(N/mm2)",1.71){ PropertyChanged=handler},
-                new SettingViewModel("HNTKYQD","混凝土抗压强度(N/mm2)",19.2){ PropertyChanged=handler},
+                new SettingViewModel("LGZJ","立杆纵距(m)",1.5){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("LGHJ","立杆横距(m)",0.8){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("BJ","步距(m)",1.8){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("LQJBS","连墙件步数",2){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("LQJKS","连墙件跨数",2){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("NJJQ","内排架距墙长度(mm)",300){ PropertyChanged=handler,GroupName="布置参数"},
+                new SettingViewModel("ZJXS","扣件抗滑承载力系数",0.8){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JSJYT","脚手架用途","结构脚手架"){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("SGHZ","施工荷载均布参数(kN/m2)",3){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("SGCS","同时施工层数",2){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JBFY","基本风压(kN/m2)",0.3){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("LGFY","立杆稳定性时风压高度变化系数",0.65){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("LQJFY","连墙件强度时风压高度变化系数",0.65){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("TXXS","风荷载体型系数",1.3){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("LGZZ","每米立杆数承受的结构自重(kN)",0.1295){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JSBLB","脚手板类别","冲压钢脚手架"){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JSBZZ","脚手板自重标准值(kN/m2)",0.3){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("DJBLB","栏杆、挡板类别","栏杆、冲压钢挡脚板"){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("DJBZZ","栏杆、挡脚板自重标准值(kN/m2)",0.16){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("AQWZZ","安全设施与安全网自重标准值(kN/m2)",0.005){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("DJTLX","地基土类型","砂土"){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("DJCZL","地基承载力标准值(kPa)",140){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JCMJ","基础底面扩展面积(m2)",0.09){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JCXS","基础降低系数",1){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("JSCDXS","计算长度系数",1.5){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("LGWDXS","轴心受压立杆的稳定系数",0.188){ PropertyChanged=handler,GroupName="荷载参数"},
+                new SettingViewModel("1DSGD","该分段顶部高度(m)",14.4){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("XGXH","型钢型号",14){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("HNTBH","楼板混凝土标号","C40"){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("JMMJ","型钢截面面积(cm2)",21.5){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("LLZL","型钢理论质量(kg/m)",16.9){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("ZDBJ","转动半径ix(cm)",5.75){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("JMML","截面模量Wx(cm3)",102){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("ZDGL","转动惯量Ix(cm4)",712){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("1LQJFY","本段顶部风压高度变化系数",0.65){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("MGXTBZ","锚固段长度与悬挑段长度比值",1.3){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("HNTKLQD","混凝土抗拉强度(N/mm2)",1.71){ PropertyChanged=handler,GroupName="悬挑参数"},
+                new SettingViewModel("HNTKYQD","混凝土抗压强度(N/mm2)",19.2){ PropertyChanged=handler,GroupName="悬挑参数"},
             };
         }
 
